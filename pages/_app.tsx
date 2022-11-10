5e02579a6bf4dc4b569b51a89c4cb4437e29e359
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-11-10 23:54:10 +0100

    fix head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  <Head>
-    <title>That time I reincarnated as Jürgen and nearly found the pizza, so someone created another level</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </Head>
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>That time I reincarnated as Jürgen and nearly found the pizza, so someone created another level</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
