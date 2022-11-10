180a72fe8a927b763fb926301c3c2289e1e9c378
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-11-10 23:55:55 +0100

    favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,15 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>That time I reincarnated as Jürgen and nearly found the pizza, so someone created another level</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#2a8379" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#1e1e1e" />
+        <meta name="msapplication-config" content="/images/browserconfig.xml" />
+        <meta name="theme-color" content="#1e1e1e" />
       </Head>
       <Component {...pageProps} />
     </>
