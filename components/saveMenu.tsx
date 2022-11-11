34a157a1654d48d95fd60ff57ae947ffa8b60087
Clyde | Netzwerkpolizei™️ <74925250+FixlTV@users.noreply.github.com>
2022-11-11 16:37:01 +0100

    support for downloading maps as images

--- components/saveMenu.tsx
+++ components/saveMenu.tsx
@@ -4,11 +4,19 @@ import Button from "./button";
 
 export default function SaveMenu(props: { setVisible: any, content: any, setContent: any, data: any, width: number, height: number }) {
 
+    const loadImage = (path: string) => {
+        return new Promise((resolve) => {
+            let img = new Image()
+            img.onload = () => {
+                resolve(img)
+            }
+            img.src = path
+        })
+    }
+
     const downloadLevelFile = (e: any) => {
         e.preventDefault();
-        const input = document.getElementById('filename') as HTMLInputElement;
-        const fileName = input.value;
-        props.setContent(fileName);
+        let fileName = props.content
         if(fileName) {
             let datastring = '';
             props.data.forEach((b: any) => {
@@ -30,9 +38,7 @@ export default function SaveMenu(props: { setVisible: any, content: any, setCont
 
     const downloadJSONFile = (e: any) => {
         e.preventDefault();
-        const input = document.getElementById('filename') as HTMLInputElement;
-        const fileName = input.value;
-        props.setContent(fileName);
+        const fileName = props.content;
         if(fileName) {
             let blob = new Blob([JSON.stringify({
                 name: fileName,
@@ -50,11 +56,44 @@ export default function SaveMenu(props: { setVisible: any, content: any, setCont
         }
     }
 
+    const downloadImage = async (e: any) => {
+        e.preventDefault();
+        const fileName = props.content;
+        if(fileName) {
+            let images: any[] = [];
+            props.data.forEach((b: any) => !images.includes(`images/${b.id}.png`) ? images.push(`images/${b.id}.png`) : undefined)
+            let imageData: any = {};
+            await Promise.all(images.map(async (image) => { !imageData[image.split('/')[1].split('.')[0]] ? imageData[image.split('/')[1].split('.')[0]] = await loadImage(image) : undefined }))
+            let canvas = document.createElement('canvas');
+
+            canvas.height = props.height * 50;
+            canvas.width = props.width * 50;
+
+            let ctx = canvas.getContext('2d');
+            if(ctx == null) return;
+            ctx.imageSmoothingEnabled = false;
+            props.data.forEach((b: any) => { //@ts-ignore
+                ctx.drawImage(imageData[b.id], (b.x) * 50, (b.y) * 50, 50, 50)
+            })
+            canvas.toBlob((blob) => {
+                if(blob == null) return;
+                const element = document.createElement("a")
+                element.style.display = 'none';
+                element.href = URL.createObjectURL(blob);
+                element.download = `${fileName.replaceAll(' ', '_').toLowerCase()}.png`;
+                document.body.appendChild(element);
+                element.click();
+                document.body.removeChild(element);
+            }, 'image/png')
+        }
+    }
+
     return (
         <Interface label='That time someone downloaded the level files' setVisible={props.setVisible}>
             <InputField id="filename" label="Dateiname" defaultValue={props.content || "level"} setContent={props.setContent} forceContent={true} />
             <Button text="Level Datei herunterladen" onClick={downloadLevelFile} fillColumn={true} />
             <Button text="JSON Datei herunterladen" onClick={downloadJSONFile} fillColumn={true} />
+            <Button text="Level Bild herunterladen" onClick={downloadImage} fillColumn={true} />
         </Interface>
     )
 }
